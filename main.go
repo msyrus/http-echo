74d54457212f7218d4c51d83b2e24741eb132d06
Minhaz Ahmed Syrus <msyrus@gmail.com>
2021-07-01 23:11:29 +0100

    add support for optional header print

--- main.go
+++ main.go
@@ -2,19 +2,22 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
-var printBody bool
+var printBody, printHeaders bool
 
 func main() {
 	var host, port string
 	flag.StringVar(&host, "host", "0.0.0.0", "host name")
 	flag.StringVar(&port, "port", "8000", "port number")
 	flag.BoolVar(&printBody, "print-body", false, "print request body to stdout")
+	flag.BoolVar(&printHeaders, "print-headers", false, "print request headers to stdout")
 	flag.Parse()
 
 	addr := host + ":" + port
@@ -28,7 +31,12 @@ func main() {
 // Logger is the http request logger middleware
 func Logger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Println(r.Proto, r.Host, r.Method, r.RequestURI)
+		log.Println(r.Proto, r.RemoteAddr, r.Host, r.Method, r.RequestURI)
+		if printHeaders {
+			for k, vs := range r.Header {
+				fmt.Fprintf(os.Stdout, "%s: %s\n", k, strings.Join(vs, ", "))
+			}
+		}
 		if printBody && r.Body != nil && r.ContentLength != 0 {
 			defer func() {
 				r.Body.Close()
@@ -47,9 +55,14 @@ func EchoHandler(w http.ResponseWriter, r *http.Request) {
 			w.Header().Add("Req-"+k, v)
 		}
 	}
+
+	w.Header().Add("Req-Host", r.Host)
+	w.Header().Set("Req-Remote", r.RemoteAddr)
 	host, _ := os.Hostname()
 	w.Header().Set("Host", host)
-	w.Header().Set("Content-Type", r.Header.Get("Content-Type"))
+	if cType := r.Header.Get("Content-Type"); cType != "" {
+		w.Header().Set("Content-Type", cType)
+	}
 
 	if r.Body != nil {
 		defer r.Body.Close()
